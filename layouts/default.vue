80596ce8358e8da5663c931034d3615490c7c233
DavidsonG5 <davidsonzales@gmail.com>
2025-06-20 17:48:26 +0800

    update file

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,6 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Signin',
+          to: 'auth/signin'
         }
       ],
       miniVariant: false,
---
+++ pages/auth/signin.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="d-flex align-center justify-center" style="height: 100vh;">
+    <v-card width="500">
+  <v-form>
+    <h1> LOGIN PAGE</h1>
+    <v-text-field
+
+      v-model="name"
+      label="Email"
+    ></v-text-field>
+
+    <v-text-field
+      label="password"
+    ></v-text-field>
+
+    <v-btn block>
+    Sign in
+    </v-btn>
+
+  </v-form>
+  </v-card>
+  </div>
+</template>
+<script>
+export default {
+    layout: "auth"
+}
+</script>
